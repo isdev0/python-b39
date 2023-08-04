97b9315aca39ddfa78573a0769219a4750cec5e5
isdev0 <isdev@rambler.ru>
2023-08-04 18:15:47 +0200

    base methods extracting

--- test_add_group.py
+++ test_add_group.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Firefox()
+        #self.wd = webdriver.Firefox()
+        self.wd = webdriver.Chrome()
+        self.wd.maximize_window()
         self.wd.implicitly_wait(30)
 
     def test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys("admin")
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys("secret")
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
+        self.open_external_page(wd, "http://localhost/addressbook/")
+        self.login(wd)
+        self.open_internal_page(wd, "groups")
+        self.create_group(wd)
+        self.open_internal_page(wd, "groups")
+        self.open_internal_page(wd, "Logout")
+
+    def create_group(self, wd):
+        # create new group
         wd.find_element(By.NAME, "new").click()
+
+        # fill out group values
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys("123123")
@@ -33,10 +35,24 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys("234")
+        # save new group
         wd.find_element(By.NAME, "submit").click()
-        wd.find_element(By.LINK_TEXT, "groups").click()
-        wd.find_element(By.LINK_TEXT, "Logout").click()
-    
+
+    def open_internal_page(self, wd, tab):
+        wd.find_element(By.LINK_TEXT, tab).click()
+
+    def login(self, wd):
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def open_external_page(self, wd, url):
+        wd.get(url)
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
