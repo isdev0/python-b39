2b293077a58f9233039424451cf3c45a20c45b8e
isdev0 <isdev@rambler.ru>
2023-08-20 18:20:55 +0200

    extracting additional classes and methods

--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from group import Group
 import unittest
 
 class TestAddGroup(unittest.TestCase):
@@ -12,20 +13,20 @@ class TestAddGroup(unittest.TestCase):
         self.wd.maximize_window()
         self.wd.implicitly_wait(30)
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # create new group
         wd.find_element(By.NAME, "new").click()
 
         # fill out group values
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
-        wd.find_element(By.NAME, "group_name").send_keys(name)
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
-        wd.find_element(By.NAME, "group_header").send_keys(header)
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
-        wd.find_element(By.NAME, "group_footer").send_keys(footer)
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
 
         # save new group
         wd.find_element(By.NAME, "submit").click()
@@ -55,15 +56,6 @@ class TestAddGroup(unittest.TestCase):
         except NoAlertPresentException as e: return False
         return True
 
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_external_page(wd, "http://localhost/addressbook/")
-        self.login(wd, username="admin", password="secret")
-        self.open_internal_page(wd, "groups")
-        self.create_group(wd, name="", header="", footer="")
-        self.open_internal_page(wd, "groups")
-        self.open_internal_page(wd, "Logout")
-
 
 
     def test_add_group(self):
@@ -71,7 +63,16 @@ class TestAddGroup(unittest.TestCase):
         self.open_external_page(wd, "http://localhost/addressbook/")
         self.login(wd, username="admin", password="secret")
         self.open_internal_page(wd, "groups")
-        self.create_group(wd, name="123123", header="123", footer="234")
+        self.create_group(wd, Group(name="123123", header="123", footer="234"))
+        self.open_internal_page(wd, "groups")
+        self.open_internal_page(wd, "Logout")
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_external_page(wd, "http://localhost/addressbook/")
+        self.login(wd, username="admin", password="secret")
+        self.open_internal_page(wd, "groups")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.open_internal_page(wd, "groups")
         self.open_internal_page(wd, "Logout")
 
