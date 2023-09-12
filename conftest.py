7f7add4a7d91b319b25060de58aa224933186463
isdev0 <isdev@rambler.ru>
2023-09-12 16:04:54 +0200

    fixture login/logout validation added

--- conftest.py
+++ conftest.py
@@ -8,17 +8,16 @@ def app(request):
     global fixture
     if fixture is None:
         fixture = Application()
-        fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
             fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+    fixture.session.do_login(username="admin", password="secret")
     return fixture
 
 @pytest.fixture(scope="session", autouse=True)
 def terminate(request):
     def fin():
-        fixture.session.logout()
+        fixture.session.do_logout()
         fixture.destroy()
 
     request.addfinalizer(fin)
--- fixture/session.py
+++ fixture/session.py
@@ -25,3 +25,24 @@ class SessionHelper:
         wd.find_element(By.NAME, "user")
         #time.sleep(0.25)
 
+    def is_login(self):
+        wd = self.app.wd
+        return len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_login_as(self, username):
+        wd = self.app.wd
+        return wd.find_element(By.XPATH, "//div/div[1]/form/b").text == "(" + username + ")"
+
+    def do_login(self, username, password):
+        wd = self.app.wd
+        if self.is_login():
+            if self.is_login_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def do_logout(self):
+        wd = self.app.wd
+        if len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0:
+            self.logout()
