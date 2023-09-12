67fa28e0edeca41beeeee9715a0c1e4851300f4b
isdev0 <isdev@rambler.ru>
2023-09-12 02:08:22 +0200

    edit group test added

--- fixture/group.py
+++ fixture/group.py
@@ -15,14 +15,38 @@ class GroupHelper:
         wd.find_element(By.NAME, "group_name").click()
         wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys(group.name)
+
         wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.NAME, "group_header").clear()
         wd.find_element(By.NAME, "group_header").send_keys(group.header)
+
         wd.find_element(By.NAME, "group_footer").click()
         wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
 
         wd.find_element(By.NAME, "submit").click()
+        self.app.open_internal_page("groups")
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+
+        self.app.open_internal_page("groups")
+        wd.find_element(By.NAME, "selected[]").click()
+        wd.find_element(By.NAME, "edit").click()
+
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys(group.name)
+
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys(group.header)
+
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
+
+        wd.find_element(By.NAME, "update").click()
         self.app.open_internal_page("groups")
 
     def delete_first_group(self):
---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="999999", header="999", footer="999"))
+    app.session.logout()
