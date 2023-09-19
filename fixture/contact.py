0510727f312ca34f16da5e8acbebc61cd6660185
isdev0 <isdev@rambler.ru>
2023-09-19 19:46:04 +0200

    test_phone_on_contact_view_page test grooming

--- fixture/contact.py
+++ fixture/contact.py
@@ -149,9 +149,17 @@ class ContactHelper:
         wd = self.app.wd
         self.open_contact_by_index(index, "Details")
         text = wd.find_element(By.ID, "content").text
-        homephone       = re.search("H: (.*)", text).group(1)
-        workphone       = re.search("W: (.*)", text).group(1)
-        mobilephone     = re.search("M: (.*)", text).group(1)
-        secondaryphone  = re.search("P: (.*)", text).group(1)
+
+        try: homephone = re.search("H: (.*)", text).group(1)
+        except: homephone = ""
+
+        try: workphone = re.search("W: (.*)", text).group(1)
+        except: workphone = ""
+
+        try: mobilephone = re.search("M: (.*)", text).group(1)
+        except: mobilephone = ""
+
+        try: secondaryphone = re.search("P: (.*)", text).group(1)
+        except: secondaryphone = ""
 
         return Contact(home=homephone, mobile=mobilephone, work=workphone, phone2=secondaryphone)
