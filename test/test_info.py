f2bb067609c73f1f719b976e600ec20707ba1984
isdev0 <isdev@rambler.ru>
2023-09-19 21:08:09 +0200

    test_info_on_home_page grooming

--- test/test_info.py
+++ test/test_info.py
@@ -6,23 +6,24 @@ import re
 def test_phones_on_home_page(app):
     test_info_on_home_page(app, 0, scope=("all_phones"))
 
-def test_info_on_home_page(app, index=0, scope=None):
+def test_info_on_home_page(app, index=None, scope=None):
+    contact_count = app.contact.count()
+    if contact_count == 0:
+        app.contact.create(Contact(firstname="FN TestForInfoDisplaying", lastname="LN TestForInfoDisplaying", address="Lalaland", mobile="(111) 111-11-111", email2="email2"))
+        index = 0
+
+    if index is None:
+        index = randrange(contact_count)
+        print("\nRandom Index: " + str(index))
+
     contact_from_home_page = app.contact.get_all()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+
     if scope is None or "firstname"  in scope: assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     if scope is None or "lastname"   in scope: assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     if scope is None or "address"    in scope: assert contact_from_home_page.address == contact_from_edit_page.address
     if scope is None or "all_phones" in scope: assert contact_from_home_page.all_phones == merge_phones(contact_from_edit_page)
     if scope is None or "all_emails" in scope: assert contact_from_home_page.all_emails == merge_emails(contact_from_edit_page)
-
-def test_random_contact_info_on_home_page(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="FN TestForInfoDisplaying", lastname="LN TestForInfoDisplaying", mobile="(111) 111-11-111", email2="email2"))
-
-    index = randrange(app.contact.count())
-    print("\nRandom Index: " + str(index))
-
-    test_info_on_home_page(app, index)
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_info_from_view_page(0)
