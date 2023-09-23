9747f5daf8151e43ab6e062a1655b7499bfe30df
isdev0 <isdev@rambler.ru>
2023-09-23 13:13:26 +0200

    ORM fixture greening (change converters)

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,21 +1,34 @@
 from fixture.db import DbFixture
 from fixture.orm import ORMFixture
+from model.group import Group
 
 #db = DbFixture(host="localhost", port=3306, database="addressbook", user="root", password="")
 db = ORMFixture(host="localhost", port=3306, database="addressbook", user="root", password="")
 
 try:
-    groups = db.get_all_groups()
-    print("\n")
-    for group in groups:
-        print(group)
-    print(len(groups))
+    # groups = db.get_all_groups()
+    # print("\n")
+    # for group in groups:
+    #     print(group)
+    # print(len(groups))
+    #
+    # contacts = db.get_all_contacts()
+    # print("\n")
+    # for contact in contacts:
+    #     print(contact)
+    # print(len(contacts))
 
-    contacts = db.get_all_contacts()
+    # l = db.get_group_by_id("199")
+    # print("\n")
+    # for item in l:
+    #     print(item)
+    # print(len(l))
+
+    l = db.get_contacts_not_in_group(Group(id="199"))
     print("\n")
-    for contact in contacts:
-        print(contact)
-    print(len(contacts))
+    for item in l:
+        print(item)
+    print(len(l))
 
 finally:
     pass#db.destroy()
--- fixture/orm.py
+++ fixture/orm.py
@@ -15,16 +15,18 @@ class ORMFixture:
         name    = Optional(str,     column="group_name")
         header  = Optional(str,     column="group_header")
         footer  = Optional(str,     column="group_footer")
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = "addressbook"
-        id          = PrimaryKey(int,   column="id")
-        firstname   = Optional(str,     column="firstname")
-        lastname    = Optional(str,     column="lastname")
-        deprecated  = Optional(str,     column="deprecated") #if using conv=decoders then type should be =str, for the pony default use =datetime
+        id          = PrimaryKey(int,    column="id")
+        firstname   = Optional(str,      column="firstname")
+        lastname    = Optional(str,      column="lastname")
+        deprecated  = Optional(datetime, column="deprecated") #if using conv=decoders then type should be =str, for the pony default use =datetime
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
 
     def __init__(self, host, port, database, user, password):
-        self.db.bind("mysql", host=host, port=port, database=database, user=user, password=password, conv=decoders)
+        self.db.bind("mysql", host=host, port=port, database=database, user=user, password=password)#, conv=decoders)
         self.db.generate_mapping()
         sql_debug(True)
 
@@ -38,6 +40,14 @@ class ORMFixture:
             return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
         return list(map(convert, contacts))
 
+    def get_orm_group_by_id(self, group_id):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group_id))[0]
+        return orm_group
+
+    @db_session
+    def get_group_by_id(self, group_id="199"):
+        return self.convert_groups_to_model(select(group for group in ORMFixture.ORMGroup if group.id == group_id))
+
     @db_session
     def get_all_groups(self):
         return self.convert_groups_to_model(select(group for group in ORMFixture.ORMGroup))
@@ -45,3 +55,15 @@ class ORMFixture:
     @db_session
     def get_all_contacts(self):
         return self.convert_contacts_to_model(select(contact for contact in ORMFixture.ORMContact if contact.deprecated is None))
+
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = self.get_orm_group_by_id(group.id)
+        return self.convert_contacts_to_model(orm_group.contacts)
+
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = self.get_orm_group_by_id(group.id)
+        return self.convert_contacts_to_model(
+            select(contact for contact in ORMFixture.ORMContact if contact.deprecated is None and orm_group not in contact.groups)
+        )
