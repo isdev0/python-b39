746539fed17a7efc7b2462fe41eab373fd458ff1
isdev0 <isdev@rambler.ru>
2023-10-01 21:38:44 +0200

    allure-pytest bdd attempt

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
 
 
 def test_add_group(app, db, data_groups):
-    old_groups = db.get_all_groups()
-    app.group.create(data_groups)
-    new_groups = db.get_all_groups()
-    old_groups.append(data_groups)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step("Given a group list"):
+        old_groups = db.get_all_groups()
+
+    with pytest.allure.step("When I add the group %s to the list" % data_groups):
+        app.group.create(data_groups)
+
+    with pytest.allure.step("Then the new group list is equal to the old list with the added group"):
+        new_groups = db.get_all_groups()
+        old_groups.append(data_groups)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
