a61ad70a319ced43e7cc7857670dc8991422db53
isdev0 <isdev@rambler.ru>
2023-10-01 20:56:34 +0200

    allure-pytest added

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,9 +2,9 @@
 from model.group import Group
 
 
-def test_add_group(app, orm, data_groups):
-    old_groups = orm.get_all_groups()
+def test_add_group(app, db, data_groups):
+    old_groups = db.get_all_groups()
     app.group.create(data_groups)
-    new_groups = orm.get_all_groups()
+    new_groups = db.get_all_groups()
     old_groups.append(data_groups)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
